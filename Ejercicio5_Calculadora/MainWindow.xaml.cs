5ada290096454626e7b15528377c9eed2aa64aed
susana <susana.casas@iesdoctorbalmis.com>
2020-11-03 12:05:39 +0100

    Agregar archivos de proyecto.

---
+++ Ejercicio5_Calculadora/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Ejercicio5_Calculadora
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            numerosPulsados.Text += (sender as Button).Tag.ToString();
+        }
+    }
+}
