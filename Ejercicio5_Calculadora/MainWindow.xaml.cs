356d89f6ec063628894cf5623bc9b99e38df7723
susana <susana.casas@iesdoctorbalmis.com>
2020-11-09 17:37:17 +0100

    Creación dinámica de controles

--- Ejercicio5_Calculadora/MainWindow.xaml.cs
+++ Ejercicio5_Calculadora/MainWindow.xaml.cs
@@ -11,6 +11,33 @@ namespace Ejercicio5_Calculadora
         public MainWindow()
         {
             InitializeComponent();
+
+            const int FILAS = 3, COLUMNAS = 3;
+            int numero = 1;
+
+            for (int i = 1; i <= FILAS; i++)
+            {
+                for (int j = 0; j < COLUMNAS; j++)
+                {
+                    Button boton = new Button
+                    {
+                        Tag = numero,
+                        Content = new Viewbox
+                        {
+                            Child = new TextBlock
+                            {
+                                Text = numero.ToString()
+                            }
+                        }
+                    };
+                    Grid.SetRow(boton, i);
+                    Grid.SetColumn(boton, j);
+
+                    numeros.Children.Add(boton);
+                    numero++;
+                }
+            }
+           
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
